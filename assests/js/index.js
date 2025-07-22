e6b86122c313347f558a65f541eaf88069826b2b
riteshfauzdar63 <rfauzdar6@gmail.com>
2025-07-22 15:45:30 +0530

    added the slider carousel in the homepage

--- assests/js/index.js
+++ assests/js/index.js
@@ -14,3 +14,49 @@
       }
     });
   });
+
+  $(document).ready(function() {
+  $(".treatment-carousel").slick({
+    centerMode : true,
+    slidesToShow : 1,
+    slidesToScroll: 1, 
+    dots : true,
+    autoplay : true,
+    autoplaySpeed : 2000,
+    centerPadding :"220px",
+    arrows: false,
+    infinite: true,
+     responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          centerPadding: "180px"
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          centerPadding: "120px"
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          centerPadding: "20px"
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          centerPadding: "0px"
+        }
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          centerPadding: "0px"
+        }
+      }
+    ]
+  });
+});
